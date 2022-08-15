373ea4d8f8806f0cad25c5a9bfd492541f3b6a5c
iamtalib13 <talibsheikh16@gmail.com>
2022-08-15 21:38:41 +0530

    Added - Take More Than 3 Parameters

--- src/src/com/bridgelabz/GenericsTestMaximum.java
+++ src/src/com/bridgelabz/GenericsTestMaximum.java
@@ -1,63 +1,40 @@
 package src.com.bridgelabz;
 
-public class GenericsTestMaximum {
-    public static Integer compareTo(Integer a,Integer b,Integer c) {
-        if(a > b){
-            if(a > c) {
-                return a;
-            } else {
-                return c;
-            }
-        } else {
-            if(b > c) {
-                return b;
-            } else {
-                return c;
-            }
+import java.util.ArrayList;
+
+import java.util.Collections;
+
+public class GenericsTestMaximum<T extends Comparable<T>> {
+
+    ArrayList<T> list = new ArrayList<T>();
+
+    GenericsTestMaximum(T... inputs) {
+
+        for (T value : inputs) {
+            this.list.add(value);
         }
     }
 
-    public static Float compareTo(Float a,Float b,Float c) {
-        if(a > b){
-            if(a > c) {
-                return a;
-            } else {
-                return c;
-            }
-        } else {
-            if(b > c) {
-                return b;
-            } else {
-                return c;
-            }
-        }
+    public T testMaximum() {
+        return GenericsTestMaximum.testMaximum(list);
     }
-    public static String compareTo(String str1,String str2,String str3) {
-        if(str1.compareTo(str2) > 0 ) {
-            if(str1.compareTo(str3) > 0) {
-                return str1;
-            } else {
-                return str3;
-            }
-        } else {
-            if(str2.compareTo(str3) > 0) {
-                return str2;
-            } else {
-                return str3;
-            }
-        }
+
+    public static <T extends Comparable<T>> T testMaximum(ArrayList<T> list) {
+        Collections.sort(list);
+        T maxValue = list.get(list.size() - 1);
+        System.out.println("Maximum : " + maxValue);
+        return maxValue;
     }
 
     public static void main(String[] args) {
         System.out.println("Java Core - Generics Test Maximum");
-        int maxInt=compareTo(50,200,90);
-        System.out.println("Maximum from 3 Integer : "+maxInt);
-        
-        float maxFloat=compareTo(500.12F,200.23F,900.52F);
-        System.out.println("Maximum from 3 Float : "+maxFloat);
+        Integer intValue1 = 30, intValue2 = 50, intValue3 = 96, intValue4 = 102;
+        Float floatValue1 = 3.3f, floatValue2 = 3.5f, floatValue3 = 8.7f;
+        String stringValue1 = "Apple", stringValue2 = "Peach", stringValue3 = "Banana";
 
-        String maxString=compareTo("Apple","Peach","Banana");
-        System.out.println("Maximum from 3 String : "+maxString);
-        
+        new GenericsTestMaximum(intValue1, intValue2, intValue3, intValue4).testMaximum();
+        new GenericsTestMaximum(floatValue1, floatValue2, floatValue3).testMaximum();
+        new GenericsTestMaximum(stringValue1, stringValue2, stringValue3).testMaximum();
+
     }
 }
