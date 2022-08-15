a718cd6fc8b09525489f06b65b08b56418e6d5b7
iamtalib13 <talibsheikh16@gmail.com>
2022-08-15 19:29:40 +0530

    Added - Welcome Message

---
+++ src/src/com/bridgelabz/GenericsTestMaximum.java
@@ -0,0 +1,8 @@
+package src.com.bridgelabz;
+
+public class GenericsTestMaximum {
+
+    public static void main(String[] args) {
+        System.out.println("Java Core - Generics Test Maximum");
+    }
+}
