b606533bf1cd2ccaf3496c353123381a5f676f27
iamtalib13 <talibsheikh16@gmail.com>
2022-08-15 19:49:45 +0530

    Added - Maximum from 3 Float

--- src/src/com/bridgelabz/GenericsTestMaximum.java
+++ src/src/com/bridgelabz/GenericsTestMaximum.java
@@ -17,9 +17,29 @@ public class GenericsTestMaximum {
         }
     }
 
+    public static Float compareTo(Float a,Float b,Float c) {
+        if(a > b){
+            if(a > c) {
+                return a;
+            } else {
+                return c;
+            }
+        } else {
+            if(b > c) {
+                return b;
+            } else {
+                return c;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Java Core - Generics Test Maximum");
-        int max=compareTo(50,200,90);
-        System.out.println("Maximum from 3 Integer : "+max);
+        int maxInt=compareTo(50,200,90);
+        System.out.println("Maximum from 3 Integer : "+maxInt);
+        
+        float maxFloat=compareTo(500.12F,200.23F,900.52F);
+        System.out.println("Maximum from 3 Float : "+maxFloat);
+        
     }
 }
