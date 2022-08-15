e8a2d8de9f92b61a6bfe84a4b778fcb88ee1e536
iamtalib13 <talibsheikh16@gmail.com>
2022-08-15 19:36:52 +0530

    Added - Maximum from 3 Integers

--- src/src/com/bridgelabz/GenericsTestMaximum.java
+++ src/src/com/bridgelabz/GenericsTestMaximum.java
@@ -1,8 +1,25 @@
 package src.com.bridgelabz;
 
 public class GenericsTestMaximum {
+    public static Integer compareTo(Integer a,Integer b,Integer c) {
+        if(a > b){
+            if(a > c) {
+                return a;
+            } else {
+                return c;
+            }
+        } else {
+            if(b > c) {
+                return b;
+            } else {
+                return c;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Java Core - Generics Test Maximum");
+        int max=compareTo(50,200,90);
+        System.out.println("Maximum from 3 Integer : "+max);
     }
 }
